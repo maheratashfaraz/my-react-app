44d750ddac0bd68436bf5e74c1ea4cea353afcb0
maheratashfaraz <maheratashfaraz2000@yahoo.com>
2019-03-03 23:14:20 +0000

    Hello hello

--- src/PageTemplate.jsx
+++ src/PageTemplate.jsx
@@ -4,7 +4,8 @@ export class PageTemplate extends Component {
   render() {
     return (
       <div>
-        <h1>Hello world</h1>
+        <h1>Sean and Heaven</h1>
+        <h2>Sean's attitude reflects the heaven.</h2>
       </div>
     );
   }
