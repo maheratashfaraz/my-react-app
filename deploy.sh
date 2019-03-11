89154c52df8b27e90ab5c84ab6f1e9164e804767
maheratashfaraz <maheratashfaraz2000@yahoo.com>
2019-03-11 16:31:28 +0000

    WIP

--- deploy.sh
+++ deploy.sh
@@ -5,74 +5,100 @@ JQ="jq --raw-output --exit-status"
 
 configure_aws_cli(){
     aws --version
+    accesskey=${environment}accesskey
+    secretkey=${environment}secretkey
+    aws configure set aws_access_key_id ${!accesskey} --profile $environment
+    aws configure set aws_secret_access_key ${!secretkey} --profile $environment
+    aws configure set default.output json --profile $environment
+    aws configure set default.region us-east-1 --profile $environment
     aws configure set default.region us-east-1
     aws configure set default.output json
 }
 
 deploy_cluster() {
+    family="devops-test-cluster"
+    make_task_def
+    register_definition
+    if [[ $(aws ecs update-service --cluster devops-test-cluster --service devops-test-service --task-definition $revision --profile $environment --region us-east-1 | \
+                $JQ '.service.taskDefinition') != $revision ]]; then
+        echo "Error updating service."
+        return 1
+    fi
 
-   family="sample-webapp-task-family"
+    echo "Deployed!"
+    return 0
 
-   make_task_def
-   register_definition
-   if [[ $(aws ecs update-service --cluster sample-webapp-cluster --service sample-webapp-service --task-definition $revision | \
-                  $JQ '.service.taskDefinition') != $revision ]]; then
-       echo "Error updating service."
-       return 1
-   fi
-
-   # wait for older revisions to disappear
-   # not really necessary, but nice for demos
-   for attempt in {1..30}; do
-       if stale=$(aws ecs describe-services --cluster sample-webapp-cluster --services sample-webapp-service | \
-                      $JQ ".services[0].deployments | .[] | select(.taskDefinition != \"$revision\") | .taskDefinition"); then
-           echo "Waiting for stale deployments:"
-           echo "$stale"
-           sleep 5
-       else
-           echo "Deployed!"
-           return 0
-       fi
-   done
-   echo "Service update took too long."
-   return 1
+    # wait for older revisions to disappear
+    # not really necessary, but nice for demos
+    for attempt in {1..65}; do
+        if stale=$(aws ecs describe-services --cluster devops-test-cluster --services devops-test-service --profile $environment | \
+                       $JQ ".services[0].deployments | .[] | select(.taskDefinition != \"$revision\") | .taskDefinition"); then
+            echo "Waiting for stale deployments:"
+            echo "$stale"
+            sleep 5
+        else
+            echo "Deployed!"
+            return 0
+        fi
+    done
+    echo "Service update took too long."
+    return 1
 }
 
 make_task_def(){
     task_template='[
         {
-            "name": "go-sample-webapp",
-            "image": "%s.dkr.ecr.us-east-1.amazonaws.com/go-sample-webapp:%s",
+            "name": "devops-test-web",
+            "image": "%s.dkr.ecr.eu-west-1.amazonaws.com/my-react-app:%s",
             "essential": true,
-            "memory": 200,
+            "memory": 512,
             "cpu": 10,
             "portMappings": [
                 {
-                    "containerPort": 8080,
-                    "hostPort": 80
+                    "containerPort": 80,
+                    "hostPort": 0
                 }
-            ]
+            ],
         }
     ]'
-    
-    task_def=$(printf "$task_template" $AWS_ACCOUNT_ID $CIRCLE_SHA1)
+    BUILD_NUMBER=$(($CIRCLE_PREVIOUS_BUILD_NUM-2))
+    task_def=$(printf "$task_template" ${AWS_ACCOUNT_ID} $BUILD_NUMBER)
+
 }
 
 push_ecr_image(){
-    eval $(aws ecr get-login --region us-east-1 --no-include-email)
-    docker push $AWS_ACCOUNT_ID.dkr.ecr.us-east-1.amazonaws.com/go-sample-webapp:$CIRCLE_SHA1
+    aws ecr get-login --region us-east-1
+    eval $(aws ecr get-login --region us-east-1 --no-include-email | sed 's|https://||')
+    BUILD_NUMBER=$(($CIRCLE_PREVIOUS_BUILD_NUM-2))
+    docker push $AWS_ACCOUNT_ID.dkr.ecr.eu-west-1.amazonaws.com/my-react-app:$BUILD_NUMBER
+
 }
 
 register_definition() {
 
-   if revision=$(aws ecs register-task-definition --container-definitions "$task_def" --family $family | $JQ '.taskDefinition.taskDefinitionArn'); then
-       echo "Revision: $revision"
-   else
-       echo "Failed to register task definition"
-       return 1
-   fi
+    if revision=$(aws ecs register-task-definition --container-definitions "$task_def" --family $family --profile ${environment} --region us-east-1 | $JQ '.taskDefinition.taskDefinitionArn'); then
+        echo "Revision: $revision"
+    else
+        echo "Failed to register task definition"
+        exit 1
+    fi
 
 }
 
-configure_aws_cli
-push_ecr_image
+_exit_error() {
+    echo "##################################################################"
+    echo "uh oh we had an oppppsie"
+    echo ""
+    echo "see above for the error"
+    echo "##################################################################"
+    exit 1
+}
+
+echo "Configuring CLI ..."
+configure_aws_cli || _exit_error
+
+echo "Pushing image ..."
+push_ecr_image || _exit_error
+
+echo "Deploying image ..."
+deploy_cluster || _exit_error
